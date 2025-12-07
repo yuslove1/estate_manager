f436e2bf9df36964a19b328274d27f4f0648da79
yuslove1 <adexyuslove@gmail.com>
2025-12-07 09:03:49 -0500

    feat: dark mode initializer add

---
+++ src/components/DarkModeInitializer.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function DarkModeInitializer() {
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme") || "light";
+    if (savedTheme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, []);
+
+  return null;
+}
